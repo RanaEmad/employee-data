16006012c9fdff8c5e9873ab0f0ce389daf9bd3f
Rana Emad <rana3emad@gmail.com>
2019-01-20 19:27:57 +0200

    Employee class

---
+++ src/Employee.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Employee\Controllers;
+
+use Employee\Models;
+
+class Employee{
+    protected $employeeModel;
+    public function __construct() {
+        $this->employeeModel= new Models\EmployeeModel();
+    }
+    
+    public function getAllEmployees(){
+        $employees= $this->employeeModel->getAllEmployees();
+        if(!empty($employees)){
+            foreach ($employees as $employee){
+                foreach ($employee as $key=>$value){
+                    $employee->{$key}=  htmlspecialchars($value);
+                }
+            }
+        }
+        return $employees;
+    }
+
+
+    public function getEmployee(){
+        $id=$_GET["id"];
+        $employee= $this->employeeModel->getEmployee($id);
+        if(!empty($employee)){
+            foreach ($employee as $key=>$value){
+                $employee->{$key}=  htmlspecialchars($value);
+            }
+        }
+        else{
+            throw new \Exception("User Not Found");
+        }
+        return $employee;
+    }
+    
+}
